f2b60b473de26f8a474d54ae3fcaa288661068d9
misato-tp <138008284+misato-tp@users.noreply.github.com>
2024-04-10 11:21:09 +0900

    feat: :sparkles: #1 restaurantのCRUD実装

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,47 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    restaurant = Restaurant.new(restaurant_params)
+    if restaurant.save
+      redirect_to restaurants_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    restaurant = Restaurant.find(params[:id])
+    restaurant.update(restaurant_params)
+    redirect_to restaurant_path(restaurant.id)
+  end
+
+  def destroy
+    restaurant = Restaurant.find(params[:id])
+    if restaurant.destroy
+      redirect_to restaurants_path, notice: "削除が完了しました"
+    else
+      redirect_to restaurants_path, alert: "削除に失敗しました"
+    end
+  end
+end
+
+private
+
+def restaurant_params
+  params.require(:restaurant).permit(:name, :address, :image, :memo)
+end
