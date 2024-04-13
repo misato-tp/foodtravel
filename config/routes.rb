3ecadbe3deabc6786a4b7feb2c704ba53015a199
misato-tp <138008284+misato-tp@users.noreply.github.com>
2024-04-13 17:35:46 +0900

    add: :zap: #5 ゲストユーザーのパスワードやアドレスの変更を不可にする実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks",
-    registrations: "users/registrations"
+    registrations: "users/registrations",
+    passwords: "users/passwords"
   }
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
