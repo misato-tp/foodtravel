f25b0cc02285a0065045e3c1aa15d215a2ee8c7f
misato-tp <138008284+misato-tp@users.noreply.github.com>
2024-04-14 15:51:58 +0900

    add: :zap: #7 ログイン方法をアドレスからユーザーネームに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,27 @@ class UsersController < ApplicationController
   end
   
   def profile
+    @user = current_user
   end
 
   def account
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to users_profile_path
+    else
+      render users_profile_path
+    end
+  end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:username, :email, :user_id)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :validatable,
           :omniauthable, omniauth_providers: [:google_oauth2]
 
+  validates :username, presence: true
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.username = auth.info.name
@@ -14,7 +16,7 @@ class User < ApplicationRecord
   end
 
   def self.guest
-    find_or_create_by!(email: 'guest@example.com') do |user|
+    find_or_create_by!(email: 'guest@example.com', username: 'guest_user') do |user|
       user.password = SecureRandom.urlsafe_base64
       #SecureRandom.urlsafe_base64はURLに使えるランダムな文字列を生成するメソッド。
     end
