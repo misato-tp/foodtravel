1a8ee9f8c502055a070cca4dc8c88225ad90a071
misato-tp <138008284+misato-tp@users.noreply.github.com>
2024-04-14 23:07:38 +0900

    add: :zap: #13 restaurantモデルのエラーメッセージ実装

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,8 +13,8 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    restaurant = Restaurant.new(restaurant_params)
-    if restaurant.save
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
       redirect_to restaurant_path(restaurant.id)
     else
       render :new
