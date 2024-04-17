22c0b01087c8db466caac97d2cbdc37e0c646f9d
misato-tp <138008284+misato-tp@users.noreply.github.com>
2024-04-17 10:31:16 +0900

    update: :zap: restaurant/newで写真のプレビューを実装

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -2,17 +2,34 @@ document.addEventListener('DOMContentLoaded', () => {
   const createImageHTML = (blob) => {
     const imageElement = document.getElementById('preview-image');
     const blobImage = document.createElement('img');
-    blobImage.setAttribute('class', 'preview-image')
+    blobImage.setAttribute('class', 'preview-image');
     blobImage.setAttribute('src', blob);
     imageElement.appendChild(blobImage);
   };
-  document.getElementById('report_image').addEventListener('change', (e) =>{
-    const imageContent = document.querySelector('img');
-    if (imageContent){
-      imageContent.remove();
-    }
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
-    createImageHTML(blob);
-  });
+
+  const restaurantImage = document.getElementById('restaurant_image');
+  if (restaurantImage) {
+    restaurantImage.addEventListener('change', (e) => {
+      const imageContent = document.querySelector('img');
+      if (imageContent) {
+        imageContent.remove();
+      }
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      createImageHTML(blob);
+    });
+  }
+
+  const reportImage = document.getElementById('report_image');
+  if (reportImage) {
+    reportImage.addEventListener('change', (e) => {
+      const imageContent = document.querySelector('img');
+      if (imageContent) {
+        imageContent.remove();
+      }
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      createImageHTML(blob);
+    });
+  }
 });
