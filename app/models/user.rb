d3f063225d60541420576c63cd7fa15668d6e2bd
user <user@usernoMacBook-Air.local>
2024-05-07 19:51:32 +0900

    fix: :bug: #36 ゲストアカウントのpasswords_controller.rbのコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,7 @@ class User < ApplicationRecord
 
   def self.guest
     find_or_create_by!(email: 'guest@example.com', username: 'guest_user') do |user|
+      user.name = "ゲスト"
       user.password = SecureRandom.urlsafe_base64
       #SecureRandom.urlsafe_base64はURLに使えるランダムな文字列を生成するメソッド。
     end
