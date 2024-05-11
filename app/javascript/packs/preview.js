5c073f92f49cc5080888c861a21fd7cec0ea9d3b
user <user@usernoMacBook-Air.local>
2024-05-12 00:31:39 +0900

    update: :lipstick: #36 reportのレイアウト更新

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -2,34 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const createImageHTML = (blob) => {
     const imageElement = document.getElementById('preview-image');
     const blobImage = document.createElement('img');
-    blobImage.setAttribute('class', 'preview-image');
+    blobImage.setAttribute('class', 'preview-img') 
     blobImage.setAttribute('src', blob);
     imageElement.appendChild(blobImage);
   };
-
-  const restaurantImage = document.getElementById('restaurant_image');
-  if (restaurantImage) {
-    restaurantImage.addEventListener('change', (e) => {
-      const imageContent = document.querySelector('img');
-      if (imageContent) {
-        imageContent.remove();
-      }
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
-      createImageHTML(blob);
-    });
-  }
-
-  const reportImage = document.getElementById('report_image');
-  if (reportImage) {
-    reportImage.addEventListener('change', (e) => {
-      const imageContent = document.querySelector('img');
-      if (imageContent) {
-        imageContent.remove();
-      }
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
-      createImageHTML(blob);
-    });
-  }
+  document.getElementById('report_image').addEventListener('change', (e) =>{
+    const imageContent = document.querySelector('#preview-image img');
+    if (imageContent){
+      imageContent.remove();
+    }
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+    createImageHTML(blob);
+  });
 });
