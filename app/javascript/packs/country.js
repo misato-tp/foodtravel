e013f59d3990c3993a6af27f5faa32b62ee1b5e2
user <user@usernoMacBook-Air.local>
2024-05-12 15:17:21 +0900

    update: :zap: インクリメンタルサーチのコード更新

--- app/javascript/packs/country.js
+++ app/javascript/packs/country.js
@@ -1,36 +1,44 @@
-$(document).on('turbolinks:load', function() {
-  window.addEventListener("load", (e) => {
-    const inputElement = document.getElementById("restaurant_country");
-    inputElement.addEventListener('keyup', (e) => {
-      const input = document.getElementById("restaurant_country").value;
-      const xhr = new XMLHttpRequest();
-      xhr.open("GET", `search_country/?input=${input}`, true);
-      xhr.responseType = "json";
-      xhr.send();
-      xhr.onload = () => {
-        const countryName = xhr.response.keyword;
-        const searchResult = document.getElementById('search-result');
-        searchResult.innerHTML = '';
-        countryName.forEach(function(country){
-          const parentsElement = document.createElement('div');
-          const childElement = document.createElement("div");
+window.addEventListener('load', function() {
+  const inputElement = document.getElementById("restaurant_country");
+  inputElement.addEventListener('keyup', (e) => {
+    const input = document.getElementById("restaurant_country").value;
+    const baseUrl = window.location.origin;
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", `${baseUrl}/restaurants/search_country/?input=${input}`, true);
+    xhr.responseType = "json";
+    xhr.send();
+    xhr.onload = () => {
+      const countryName = xhr.response.keyword;
+      const searchResult = document.getElementById('search-result');
+      searchResult.innerHTML = '';
+      countryName.forEach(function(country){
+        const parentsElement = document.createElement('div');
+        const childElement = document.createElement("div");
 
-          parentsElement.setAttribute('id', 'parents');
-          childElement.setAttribute('id', country.id);
-          childElement.setAttribute('class', 'child');
+        parentsElement.setAttribute('id', 'parents');
+        childElement.setAttribute('id', country.id);
+        childElement.setAttribute('class', 'child');
 
-          parentsElement.appendChild(childElement);
-          childElement.innerHTML = country.name;
-          searchResult.appendChild(parentsElement);
+        parentsElement.appendChild(childElement);
+        childElement.innerHTML = country.name;
+        searchResult.appendChild(parentsElement);
 
-          const clickElement = document.getElementById(country.id);
-          clickElement.addEventListener('click', () => {
-            document.getElementById("restaurant_country").value = clickElement.textContent;
-            document.getElementById("restaurant_country_id").value = country.id;
-            clickElement.remove();
-          });
+        const clickElement = document.getElementById(country.id);
+        clickElement.addEventListener('click', () => {
+          document.getElementById("restaurant_country").value = clickElement.textContent;
+          document.getElementById("restaurant_country_id").value = country.id;
+
+          countryName.forEach(function(c) {
+            if (c.id !== country.id) {
+              const el = document.getElementById(c.id);
+              if (el) {
+                el.remove();
+                clickElement.remove();
+              }
+            }
+          }) ;
         });
-      };
-    });
+      });
+    };
   });
 });
