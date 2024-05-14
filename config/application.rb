90c97e049fa2cd3d7d1067bea8fcd916daf3b1f3
misato-tp <tommy21miko15@gmail.com>
2024-05-14 12:01:34 +0900

    fix: :lipstick: #36 ユーザー新規登録時にバリデーションでレイアウトが崩れる点を修正

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Foodtravel0409
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
     config.i18n.load_path+=Dir[Rails.root.join('config','locales','**','*.yml').to_s]
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end
