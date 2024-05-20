6b8d250822b6a556c1482cc359d5079e33bae731
misato-tp <tommy21miko15@gmail.com>
2024-05-21 00:23:15 +0900

    fix: #72 MySQLにDBを変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2024_05_01_124640) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "countries", force: :cascade do |t|
+  create_table "countries", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "likes", force: :cascade do |t|
+  create_table "likes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "restaurant_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -30,7 +27,7 @@ ActiveRecord::Schema.define(version: 2024_05_01_124640) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table "reports", force: :cascade do |t|
+  create_table "reports", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
     t.string "image"
     t.string "recommend"
@@ -41,7 +38,7 @@ ActiveRecord::Schema.define(version: 2024_05_01_124640) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "restaurants", force: :cascade do |t|
+  create_table "restaurants", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.integer "postal_code", null: false
     t.string "address", null: false
@@ -56,7 +53,7 @@ ActiveRecord::Schema.define(version: 2024_05_01_124640) do
     t.index ["country_id"], name: "index_restaurants_on_country_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
