0bc0197d1136c4d7e807bd3811da6ea21c01f4ae
misato-tp <tommy21miko15@gmail.com>
2024-06-04 21:21:54 +0900

    add: #42 Missing host to link toの修正

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,17 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV['APP_EMAIL'],
+    :password => ENV['EMAIL_PASSWORD'],
+    :authentication => 'login'
+  }
 end
