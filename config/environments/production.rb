c8ffc481095a293f79d74ebeade525a5ae54d383
misato-tp <tommy21miko15@gmail.com>
2024-06-05 09:34:34 +0900

    add: #42 本番環境のhostやドメインの情報追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,8 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  host = 'db'
-  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.default_url_options = { host: 'foodtravel-5c7b1abd1960.herokuapp.com', protocol: 'http' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
