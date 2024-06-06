fe1c53ea511058a5cde93f50e1016312eb0a22e5
misato-tp <tommy21miko15@gmail.com>
2024-06-06 23:53:06 +0900

    add: #49 各modelのFactoryを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :user do
+    email { "test@email" }
+    username { "testuser" }
+    profile_image { [ Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/test.jpg'), 'image/jpeg') ] }
+    password { "password" }
+    password_confirmation { "password" }
   end
 end
