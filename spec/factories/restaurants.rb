6f2147c10541b939dd24e712b8647b83b745f41a
misato-tp <tommy21miko15@gmail.com>
2024-06-07 11:51:38 +0900

    add: #49 models/like_specにテスト追加

--- spec/factories/restaurants.rb
+++ spec/factories/restaurants.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :restaurant do
-    name { "テストのお店" }
+    sequence(:name) { |n| "テスト#{n}のお店" }
     postal_code { "1000000" }
     address { "東京都千代田区" }
-    image { [ Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/test.jpg'), 'image/jpeg') ] }
+    image { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/test.jpg'), 'image/jpeg') }
     latitude { "35.685780" }
     longitude { "139.744959" }
     memo { "テストです" }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    email { "test@email" }
+    sequence(:email) { |n| "test#{n}@email.com" }
     username { "testuser" }
-    profile_image { [ Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/test.jpg'), 'image/jpeg') ] }
+    profile_image { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/test.jpg'), 'image/jpeg') }
     password { "password" }
     password_confirmation { "password" }
   end
