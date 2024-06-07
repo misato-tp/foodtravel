4c55e7a245801db7f99d07e3d59f11e5f2addf4c
misato-tp <tommy21miko15@gmail.com>
2024-06-07 20:04:41 +0900

    add: #49 models/restaurant_spec.rbのリレーションに関するテスト追加

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  let(:restaurant) { create(:restaurant) }
+  let(:restaurant) { create(:restaurant, reports: [report1, report2]) }
   let(:report1) { create(:report) }
   let(:report2) { create(:report) }
+  let(:like1) { create(:like, restaurant: restaurant) }
+  let(:like2) { create(:like, restaurant: restaurant) }
 
   describe 'バリデーションに関するテスト' do
     context '投稿可能な場合' do
@@ -31,18 +33,28 @@ RSpec.describe Restaurant, type: :model do
         restaurant.valid?
         expect(restaurant.errors[:address]).to include('を入力してください')
       end
+    end
+  end
 
-      it 'userなしでは登録が無効であること' do
-        restaurant = build(:restaurant, user: nil)
-        restaurant.valid?
-        expect(restaurant.errors[:user]).to include('を入力してください')
-      end
+  describe 'リレーションに関するテスト' do
+    it 'userなしでは登録ができないこと' do
+      restaurant = build(:restaurant, user: nil)
+      restaurant.valid?
+      expect(restaurant.errors[:user]).to include('を入力してください')
+    end
 
-      it 'countryなしでは登録が無効であること' do
-        restaurant = build(:restaurant, country: nil)
-        restaurant.valid?
-        expect(restaurant.errors[:country]).to include('を入力してください')
-      end
+    it 'countryなしでは登録ができないこと' do
+      restaurant = build(:restaurant, country: nil)
+      restaurant.valid?
+      expect(restaurant.errors[:country]).to include('を入力してください')
+    end
+
+    it 'restaurantが複数のレポートを取得できること' do
+      expect(restaurant.reports).to include(report1, report2)
+    end
+    
+    it 'restaurantが複数のいいねを取得できること' do
+      expect(restaurant.likes).to include(like1, like2)
     end
   end
 end
