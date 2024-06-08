008c95a027a65af33e3a0c05eb351bf6355d99c9
misato-tp <tommy21miko15@gmail.com>
2024-06-08 22:16:16 +0900

    add: #49 models/user_spec.rbにリレーションに関するテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { create(:user) }
+  let(:restaurant1) { create(:restaurant, user: user) }
+  let(:restaurant2) { create(:restaurant, user: user) }
+  let(:like1) { create(:like, user: user) }
+  let(:like2) { create(:like, user: user) }
+  let(:report1) { create(:report, user: user) }
+  let(:report2) { create(:report, user: user) }
   
   describe 'バリデーションに関するテスト' do
     context '登録に成功する場合' do
@@ -35,6 +41,30 @@ RSpec.describe User, type: :model do
         user.valid?
         expect(user.errors[:password_confirmation]).to include('が内容とあっていません。')
       end
+    end
+  end
+
+  describe 'リレーションに関するテスト' do
+    it 'userは複数のrestaurantを取得できること' do
+      expect(user.restaurants).to include(restaurant1, restaurant2)
+    end
+
+    it 'userは複数のreportを取得できること' do
+      expect(user.reports).to include(report1, report2)
+    end
+
+    it 'userは複数のlikeを取得できること' do
+      expect(user.likes).to include(like1, like2)
+    end
+
+    it 'userを削除すると、reportも削除されること' do
+      user = create(:user, reports: [report1, report2])
+      expect{ user.destroy }.to change{ Report.count }.by(-2)
+    end
+
+    it 'userを削除すると、likeも削除されること' do
+      like1 = create(:like, user: user)
+      expect{ user.destroy }.to change{ Like.count }.by(-1)
     end
   end
 end
