34253db16852fa6697194389158539797e21e488
misato-tp <tommy21miko15@gmail.com>
2024-06-19 17:51:27 +0900

    add: #151 バリデーション追加に伴いmodels/restaurant_spec.rbにテスト追加

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -32,11 +32,11 @@ class Restaurant < ApplicationRecord
     cities = ['市', '区', '町', '村']
 
     unless address.present? && prefectures.any? { |prefecture| address.include?(prefecture) }
-      errors.add(:address, "に都道府県名を入力してください。")
+      errors.add(:address, "に都道府県名を入力してください")
     end
 
     unless address.present? && cities.any? { |city| address.include?(city) }
-      errors.add(:address, "に市区町村名を入力してください。")
+      errors.add(:address, "に市区町村名を入力してください")
     end
   end
 
