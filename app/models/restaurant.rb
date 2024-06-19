937900446686e87d04c5e99316805b1b84966cd2
misato-tp <tommy21miko15@gmail.com>
2024-06-19 17:04:16 +0900

    add: #151 restaurantのバリデーション追加

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,8 @@ class Restaurant < ApplicationRecord
     validates :name, :postal_code, :address
   end
   validate :name_uniqueness_check
+  validate :address_accurary_check
+  validate :postal_code_accurary_check
 
   mount_uploader :image, ImageUploader
 
@@ -17,9 +19,37 @@ class Restaurant < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
+  private
+
   def name_uniqueness_check
     if name.present? && Restaurant.where.not(id: id).exists?(name: name)
       errors.add(:name, "はすでに登録されているようです。お店を探してレポを書こう！")
+    end
+  end
+
+  def address_accurary_check
+    prefectures = ['都', '道', '府', '県']
+    cities = ['市', '区', '町', '村']
+
+    unless address.present? && prefectures.any? { |prefecture| address.include?(prefecture) }
+      errors.add(:address, "に都道府県名を入力してください。")
+    end
+
+    unless address.present? && cities.any? { |city| address.include?(city) }
+      errors.add(:address, "に市区町村名を入力してください。")
+    end
+  end
+
+  def postal_code_accurary_check
+    return if postal_code.blank?
+    postal_code_str = postal_code.to_s
+
+    unless postal_code_str.length == 7
+      errors.add(:postal_code, "は7桁入力してください")
+    end
+
+    unless postal_code_str.match?(/\A\d+\z/)
+      errors.add(:postal_code, "には数字のみ入力してください")
     end
   end
 end
